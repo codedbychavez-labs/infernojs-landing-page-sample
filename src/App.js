d51e0c8e3cf18d1cbf62e53a0510fc52c8e2b06f
Chavez Harris <codedbychavez@gmail>
2022-09-30 17:06:45 -0300

    Updated landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import { Component } from 'inferno';
-import { SiteNavigation } from './components/Navigation';
+import { NavigationBar } from './components/NavigationBar';
+import { Hero } from './components/Hero';
+import { NewsletterCTA } from './components/NewsletterCTA';
+import { FilterTabs } from './components/FilterTabs';
+import { BlogPostsThumbnails } from './components/BlogPostsThumbnails';
+import { Footer } from './components/Footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <SiteNavigation />
+      <div className="App page-container">
+        <NavigationBar />
+        <Hero />
+        <NewsletterCTA />
+        <FilterTabs />
+        <BlogPostsThumbnails />
+        <Footer />
       </div>
     );
   }
